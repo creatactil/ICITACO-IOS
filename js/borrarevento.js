0b40e000b5087d47b6116162cf18a2093f5762a3
Gines <venazcocabrera@gmail.com>
2016-04-19 11:21:01 +0100

    beven

---
+++ js/borrarevento.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("div[id=micalendario]").click(function () {
+	//$("#idcapa").click(function () {
+	//$("div[class=clasecapa]").click(function () {
+	//$(".clasecapa").click(function () {
+	//alert("has hecho click en la capa div");
+	$('#listadeeventos').empty();
+	});
+});
